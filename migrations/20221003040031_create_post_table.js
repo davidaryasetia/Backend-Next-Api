5c5b4da719d36807c929688ad969c50a30b4850a
davidaryasetia <david.aryasetia@gmail.com>
2022-10-04 10:03:48 +0700

    next-api

---
+++ migrations/20221003040031_create_post_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.scheme.createTable("posts", function (table) {
+    table.increments();
+    table.string("tittle");
+    table.text("content");
+    table.timestamp(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("posts");
+};
---
+++ pages/about.js
@@ -0,0 +1,3 @@
+export default function about() {
+  return <h1>Hai Saya About</h1>;
+}
