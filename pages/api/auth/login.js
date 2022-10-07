841e57bd66082eec1d2b0ae8c47a41d9a2a7c3be
davidaryasetia <david.aryasetia@gmail.com>
2022-10-07 12:24:56 +0700

    api-auth

---
+++ pages/api/auth/login.js
@@ -0,0 +1,46 @@
+import db from "../../../libs/db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end;
+
+  const { email, password } = JSON.parse(req.body);
+
+  const checkUser = await db("users").where({ email }).first();
+  //   Check User jika datanya tidak ada
+  //   console.log(checkUser);
+
+  /*
+  Check logic jika datanya tidak ada(undifined) maka
+  akan unthorized
+  status(401) => unthorized
+  */
+  if (!checkUser) return res.status(401).end();
+
+  //   Bikin Pengecekan Bcrypt
+  const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+  /*
+  Check Logic jika password salah maka status(401) => authorized
+  */
+  if (!checkPassword) return res.status(401).end();
+
+  // Proses Sign In Karena dia function synchronous jadi tidak perlu pakai await
+  const token = jwt.sign(
+    {
+      id: checkUser.id,
+      email: checkUser.email,
+    },
+    "nonamanis",
+    {
+      expiresIn: "2 days",
+    }
+  );
+
+  res.status(200);
+  res.json({
+    message: "Login Successfully",
+    token,
+  });
+}
---
+++ pages/api/auth/register.js
@@ -0,0 +1,33 @@
+import db from "../../../libs/db";
+import bcrypt from "bcryptjs";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  /*
+Jika data yang dikirim dalam bentuk application/json
+maka data yang dikirim berbentuk string & string ndak bisa
+di read
+*/
+  const { email, password } = JSON.parse(req.body);
+  //   test request body
+  //   console.log({ email, password });
+
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(password, salt);
+
+  const register = await db("users").insert({
+    email,
+    password: passwordHash,
+  });
+
+  const registeredUser = await db("users").where({ id: register }).first();
+
+  //   test message registered user
+  console.log(registeredUser);
+  res.status(200);
+  res.json({
+    message: "Message register Sucsessfully",
+    data: registeredUser,
+  });
+}
